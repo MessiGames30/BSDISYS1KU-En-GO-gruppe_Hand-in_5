015744d91c15296283e69ba00cc87c8ebc83cc1c
nicklasbns <nicklasbns@gmail.com>
2024-11-19 12:38:31 +0100

    client and server in server.go works now

---
+++ startServers.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	pb "BSDISYS1KU-En-GO-gruppe_Hand-in_5/biddybidderpb"
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+
+	conn, err := grpc.NewClient("127.0.0.2:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewAuctionClient(conn)
+	message, err := client.StartFunction(context.Background(), &pb.Empty{})
+	log.Println(message)
+}
