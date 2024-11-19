3700ea279cf3aa443c946f0a26643c8ada236f00
Marius Larsen <54457839+MessiGames30@users.noreply.github.com>
2024-11-19 10:46:03 +0100

    Backup and createauction made

--- server.go
+++ server.go
@@ -3,7 +3,9 @@ package main
 import (
 	pb "BSDISYS1KU-En-GO-gruppe_Hand-in_5/biddybidderpb"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net"
@@ -21,6 +23,12 @@ type server struct {
 	targetAddress int
 	started       bool
 	client        pb.ConsensusClient
+}
+
+type Auction struct {
+	AuctionId   int       `json:"name"`
+	CurrentBid  int       `json:"type"`
+	TimeCreated time.Time `json:"Age"`
 }
 
 func main() {
@@ -125,4 +133,105 @@ func connectToServer(address int) (*pb.SuccessStart, pb.BiddyBidderClient) {
 	message, err := client.StartFunction(context.Background(), &pb.Empty{})
 	return message, client
 
+}
+
+func OngoingAuctions(ey &empty) (&Auction, error) {
+	// Scan ./Auctions for files
+	// Read the files and return the data
+	// If no files are found return an error
+	files, err := ioutil.ReadDir("./Auctions")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(files) == 0 {
+		return nil, errors.New("No ongoing auctions")
+	}
+
+	
+
+	for _, file := range files {
+	
+	}
+
+	file, err := os.Open("./Auctions/" + strconv.Itoa(len(files)) + ".json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	data := Auction{}
+	jsonData, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(jsonData, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &data, nil
+	
+}
+
+func createAuctionFile() (int, error) {
+	// Scane ./Auctions for files creates
+	// If no file is found create a new file with the name 1.json
+	// If a file is found create a new file with the name of the last file + 1
+	// Write the auction to the file
+
+	files, err := ioutil.ReadDir("./Auctions")
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, err := os.Create("./Auctions/" + strconv.Itoa(len(files)+1) + ".json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	data := Auction{
+		AuctionId:   len(files) + 1,
+		CurrentBid:  0,
+		TimeCreated: time.Now(),
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = file.Write(jsonData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return len(files) + 1, nil // IF PROBLEM WITH AUCTION ID FIX THIS
+}
+
+func backupAuction(auction Auction) error {
+
+	id := auction.AuctionId
+	fileNumber := 0
+	files, err := ioutil.ReadDir("./Auctions")
+
+	if id == len(files) {
+		fileNumber = 1
+	} else {
+		fileNumber = id + 1
+	}
+
+	file, err := os.Open("./Auctions/" + strconv.Itoa(fileNumber) + ".json")
+
+	jsonData, err := json.Marshal(auction)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = file.Write(jsonData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
 }
